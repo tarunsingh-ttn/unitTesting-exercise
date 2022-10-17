4a5411f4233f78514c72c715532b23d91cf9766c
tarunsingh-ttn <114075154+tarunsingh-ttn@users.noreply.github.com>
2022-10-17 07:49:47 +0530

    Delete src directory

--- src/main/java/com/demo/service/TestService.java
+++
@@ -1,7 +0,0 @@
-package com.demo.service;
-
-public class TestService {
-    public int test() {
-        return 1;
-    }
-}
--- src/test/java/com/demo/service/EmailServiceTest.java
+++
@@ -1,9 +0,0 @@
-package com.demo.service;
-
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public class EmailServiceTest {
-
-}
